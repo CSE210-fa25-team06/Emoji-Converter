ebed845f26b7fc8f00c5055bfa7a77af01add747
lei <leh013@ucsd.edu>
2025-10-16 01:14:38 +0800

    ininstance check

--- server/to_emoji.py
+++ server/to_emoji.py
@@ -1,12 +1,16 @@
 def phrase_to_emoji_scanner(text, pattern, phrase_to_emoji_map):   
+    if not text or not isinstance(text, str):
+        return ""
     result = []
     i = 0
     while i < len(text):
         match = pattern.match(text[i:])
         if match:
             phrase = match.group(1)
-            result.append(phrase_to_emoji_map[phrase])
-            i += len(phrase)
+            #ininstance check
+            if phrase in phrase_to_emoji_map:
+                result.append(phrase_to_emoji_map[phrase])
+                i += len(phrase)
         else:
             i += 1
     return ''.join(result)
