3516b05e9787a76fc2be4d2f706d0c65a5d25fda
Jared Ziv <jaredaziv@gmail.com>
2025-10-15 15:17:22 -0700

    Created script to get translation from backend when button is pressed

---
+++ script/call_translation.js
@@ -0,0 +1,44 @@
+/*
+This script assumes the existence of several variables present in update_mode.js: 
+emojiRadio, textRadio, inputField and outputField
+As such, it should be instantiated after update_mode.js in the HTML
+*/
+
+//get reference to translate button
+const translateButton = document.getElementById("translate-button");
+
+//define translation logic (API calls)
+async function translate() {
+    const input = inputField.value;
+    let response;
+    if (emojiRadio.checked) { //from emoji to text
+        response = await fetch("/convertToText", {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({text: input})
+        });
+    }
+    else { //from text to emoji
+        response = await fetch ("/convertToEmoji", {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({phrase: input})
+        });
+    }
+
+    const result = await response.json();
+
+    //get converted text if converting from emoji, converted emoji if otherwise
+    if (emojiRadio.checked && result.converted_text){
+        outputField.textContent = result.converted_text
+    }
+    else if (textRadio.checked && result.converted_emoji){
+        outputField.textContent = result.converted_emoji
+    }
+    else {
+        outputField.textContent = result.error;
+    }
+}
+
+//add listener to wait for click
+translateButton.addEventListener("click", translate())
